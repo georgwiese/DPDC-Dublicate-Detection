bfc3bf918da99f1d4374722c2dc192b1e67b28d0
Georg Wiese <georgwiese@gmail.com>
2015-01-24 18:04:27 +0100

    SMN: Bug fixes

--- source/core/snm.py
+++ source/core/snm.py
@@ -8,11 +8,11 @@ class SortedNeighborhoodMethod(object):
 
   def find_duplicates(self):
     result = []
-    sorted_data = sorted(self.data, key=self.compare)
+    sorted_data = sorted(self.data, cmp=self.compare)
 
-    for i in range(0, len(sorted_data) - window_size):
-      for j in range(i + 1, i + window_size):
-        if is_same(sorted_data[i], sorted_data[j]):
+    for i in range(0, len(sorted_data) - self.window_size):
+      for j in range(i + 1, i + self.window_size):
+        if self.is_same(sorted_data[i], sorted_data[j]):
           result.append((sorted_data[i][0], sorted_data[j][0]))
 
     return result
