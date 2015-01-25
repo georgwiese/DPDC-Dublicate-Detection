6d3e605cd3a2d9fa9587385bc72afb091b89d034
Georg Wiese <georgwiese@gmail.com>
2015-01-25 14:55:00 +0100

    Remove unneded functionality from analyzer

--- source/core/analyzers.py
+++ source/core/analyzers.py
@@ -1,17 +1,11 @@
 
 class Analyzer(object):
 
-  def compare(self, tuple1, tuple2):
-    pass
-
   def is_same(self, tuple1, tuple2):
     pass
 
 
 class DummyAnalyzer(Analyzer):
-
-  def compare(self, tuple1, tuple2):
-    return tuple1[0] - tuple2[0]
 
   def is_same(self, tuple1, tuple2):
     return tuple1[0] % 10 == 0 and tuple2[0] % 10 == 0
