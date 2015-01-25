fbb7736984d62edc46d29dccece5f32b04b413f8
Georg Wiese <georgwiese@gmail.com>
2015-01-25 18:07:34 +0100

    Bug fixes

--- source/core/analyzers.py
+++ source/core/analyzers.py
@@ -6,9 +6,11 @@ class Analyzer(object):
   def is_same(self, tuple1, tuple2): raise NotImplementedError()
 
   def combine_weighted(self, features):
+    weighted_features = [(weight, weight * feature)
+                            for (weight, feature) in features]
     def sum((weight1, feature1), (weight2, feature2)):
       return (weight1 + weight2, feature1 + feature2)
-    (weightSum, featureSum) = reduce(sum, features)
+    (weightSum, featureSum) = reduce(sum, weighted_features)
     return featureSum / weightSum
 
   def compare_distinct_values(self, string1, string2, base_weight):
@@ -25,7 +27,7 @@ class Analyzer(object):
     for token1 in tokenset1:
       best_similarity = 0
       for token2 in tokenset2:
-        best_similarity = max(max_similarity, sim(token1, token2))
+        best_similarity = max(best_similarity, sim(token1, token2))
       best_similarities.append(best_similarity)
     return sum(best_similarities) / len(best_similarities)
 
@@ -41,19 +43,20 @@ class AddressesGoldAnalyzer(Analyzer):
 
 class AbstractAddressesAnalyzers(Analyzer):
 
-  SALUTATION_WEIGHT = 50
+  SALUTATION_WEIGHT = 10
   TITLE_WEIGHT = 10
   NAME_WEIGHT = 50
-  THRESHOLD = 0.9
+  THRESHOLD = 0.85
 
   def is_same(self, tuple1, tuple2):
-    return self.combine_weighted([
+    similarity = self.combine_weighted([
       self.salutation_similarity(tuple1, tuple2),
       self.title_similarity(tuple1, tuple2),
       self.name_similarity(tuple1, tuple2),
       self.address_similarity(tuple1, tuple2),
       self.tel_similarity(tuple1, tuple2),
-    ]) > THRESHOLD
+    ])
+    return similarity > self.THRESHOLD
 
   def salutation_similarity(self, tuple1, tuple2): raise NotImplementedError()
   def title_similarity(self, tuple1, tuple2): raise NotImplementedError()
@@ -64,7 +67,7 @@ class AbstractAddressesAnalyzers(Analyzer):
 class AddressesAnalyzer(AbstractAddressesAnalyzers):
 
   def __init__(self):
-    self.name_tokenizer = tokenizers.NameTokenizers
+    self.name_tokenizer = tokenizers.NameTokenizer()
 
   def salutation_similarity(self, tuple1, tuple2):
     return self.compare_distinct_values(tuple1[1], tuple2[1], self.SALUTATION_WEIGHT)
@@ -73,8 +76,8 @@ class AddressesAnalyzer(AbstractAddressesAnalyzers):
     return self.compare_distinct_values(tuple1[1], tuple2[1], self.TITLE_WEIGHT)
 
   def name_similarity(self, tuple1, tuple2):
-    tokenset1 = self.name_tokenizer(tuple1[3], tuple1[4])
-    tokenset2 = self.name_tokenizer(tuple2[3], tuple2[4])
+    tokenset1 = self.name_tokenizer.tokenize(tuple1[3], tuple1[4])
+    tokenset2 = self.name_tokenizer.tokenize(tuple2[3], tuple2[4])
     similarity = self.monge_elkan(tokenset1, tokenset2, Levenshtein.ratio)
     return (self.NAME_WEIGHT, similarity)
 
